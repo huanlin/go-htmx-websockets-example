514b74e9d9f46a25f13e157051a379b5bc731601
Michael Tsai <huanlin.tsai@gmail.com>
2024-12-01 22:49:39 +0800

    Added websocket code.

--- cmd/main.go
+++ cmd/main.go
@@ -1,26 +1,81 @@
 package main
 
 import (
+	"context"
 	"fmt"
 	"net/http"
 	"os"
+	"sync"
 	"time"
 
+	"github.com/coder/websocket"
 	"github.com/huanlin/go-htmx-websockets-example/internal/hardware"
 )
 
 type server struct {
 	subscriberMessageBuffer int
 	mux                     http.ServeMux
+	subscribersMutex        sync.Mutex
+	subscribers             map[*subscriber]struct{}
+}
+
+type subscriber struct {
+	msgs chan []byte
 }
 
 func NewServer() *server {
 	s := &server{
 		subscriberMessageBuffer: 10,
+		subscribers:             make(map[*subscriber]struct{}),
 	}
 
 	s.mux.Handle("/", http.FileServer(http.Dir("./htmx")))
+	s.mux.HandleFunc("/ws", s.subscribeHandler)
 	return s
+}
+
+func (s *server) subscribeHandler(writer http.ResponseWriter, req *http.Request) {
+	err := s.subscribe(req.Context(), writer, req)
+	if err != nil {
+		fmt.Println(err)
+	}
+}
+
+func (s *server) addSubscriber(sub *subscriber) {
+	s.subscribersMutex.Lock()
+	s.subscribers[sub] = struct{}{}
+	s.subscribersMutex.Unlock()
+	fmt.Println("Added subscriber", sub)
+}
+
+func (s *server) subscribe(ctx context.Context, writer http.ResponseWriter, req *http.Request) error {
+	var wsConn *websocket.Conn
+	sub := &subscriber{
+		msgs: make(chan []byte, s.subscriberMessageBuffer),
+	}
+	s.addSubscriber(sub)
+
+	wsConn, err := websocket.Accept(writer, req, nil)
+	if err != nil {
+		return err
+	}
+	defer wsConn.CloseNow()
+
+	ctx = wsConn.CloseRead(ctx)
+
+	for {
+		select {
+		case msg := <-sub.msgs:
+			ctx, cancel := context.WithTimeout(ctx, time.Second)
+			defer cancel()
+			err := wsConn.Write(ctx, websocket.MessageText, msg)
+			if err != nil {
+				return err
+			}
+		case <-ctx.Done():
+			return ctx.Err()
+		}
+	}
 }
 
 func main() {
